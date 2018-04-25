38f86b68dda1e4831ec390915153738611fd964d
Mukul Jaggi <mkujaggi@gmail.com>
2018-04-25 11:26:22 +0530

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express=require('express');
+const http=require('http');
 const path=require('path');
+const socketIO=require('socket.io');
 
+const port=process.env.PORT || 3000;
+const rootURL=(port==3000) ?'http://localhost:3000/':'https://arcane-stream-39208.herokuapp.com/';
+;
 const publicPath=path.join(__dirname,'../public');
 var app=express();
-const port=process.env.PORT || 3000;
+var server=http.createServer(app);
+var io=socketIO(server);
 app.use(express.static(publicPath));
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+    console.log('New user connected');
+    socket.on('disconnect',()=>{
+        console.log('disconnected from client')
+    });
+});
+server.listen(port,()=>{
     console.log(`started on port ${port}`);
 });
