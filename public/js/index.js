8d3c359241aa05a83943509e17ab69cfff5588d4
Mukul Jaggi <mkujaggi@gmail.com>
2018-04-26 13:43:02 +0530

    add form for messages and show incoming messages in brouser

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,6 @@
 var socket=io();
 socket.on('connect',function(){
     console.log('connected to server');
-    // socket.emit('createMessage',{
-    //     to:'kk@abc.com',
-    //     text:'Sendind test message'
-    // });
 });
 socket.on('disconnect',function(){
     console.log('Disconnected from server');
@@ -12,10 +8,18 @@ socket.on('disconnect',function(){
 
 socket.on('newMessage',function(message){
     console.log('new Message.',message);
+    var li=$('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    $('#messages').append(li);
 });
-socket.on('newUser',function(message){
-    console.log(': ',message);
-});
-socket.on('userJoined',function(message){
-    console.log(': ',message);
+
+$(document).on('submit','#message-form',function(e){
+    e.preventDefault();
+    socket.emit('createMessage',{
+        from:'User',
+        text:$('[name=message]').val()
+    },function(){
+
+    });
+    $('[name=message]').val('')
 });
--- server/server.js
+++ server/server.js
@@ -12,12 +12,13 @@ var server=http.createServer(app);
 var io=socketIO(server);
 app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
-    socket.emit('newUser',generateMessage('Admin','Welcome to the chat room'));
-    socket.broadcast.emit('userJoined',generateMessage('Admin','New user joined'));
-    socket.on('createMessage',(newMessage)=>{
+    socket.emit('newMessage',generateMessage('Admin','Welcome to the chat room'));
+    socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
+    socket.on('createMessage',(newMessage,callback)=>{
         console.log('Created Message: ',newMessage);
         
         io.emit('newMessage',generateMessage(newMessage.from,newMessage.text));
+        callback('Acknowledgment from server');
         // socket.broadcast.emit('newMessage',{
         //     from:newMessage.from,
         //     text:newMessage.text,
