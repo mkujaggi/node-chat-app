6c4a931f5884834eb0441f1a576207484270027e
Mukul Jaggi <mkujaggi@gmail.com>
2018-04-26 10:04:11 +0530

    Add new message and create message events

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+var socket=io();
+socket.on('connect',function(){
+    console.log('connected to server');
+    socket.emit('createMessage',{
+        to:'kk@abc.com',
+        text:'Sendind test message'
+    });
+});
+socket.on('disconnect',function(){
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage',function(message){
+    console.log('new Message.',message);
+});
--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const socketIO=require('socket.io');
 
 const port=process.env.PORT || 3000;
 const rootURL=(port==3000) ?'http://localhost:3000/':'https://arcane-stream-39208.herokuapp.com/';
-;
 const publicPath=path.join(__dirname,'../public');
 var app=express();
 var server=http.createServer(app);
@@ -13,6 +12,19 @@ var io=socketIO(server);
 app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
     console.log('New user connected');
+    socket.emit('newEmail',{
+        from: 'mku@abc.com',
+        text:'some email text',
+        subject:'Random Email'
+    });
+    socket.emit('newMessage',{
+        to:'test@abc.com',
+        text:'hello from server',
+        createdAt:new Date()
+    });
+    socket.on('createMessage',(newMessage)=>{
+        console.log('Created Message: ',newMessage);
+    });
     socket.on('disconnect',()=>{
         console.log('disconnected from client')
     });
