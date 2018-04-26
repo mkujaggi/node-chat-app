62df36a995ffe69bf79684d219c48b42bec2f8d5
Mukul Jaggi <mkujaggi@gmail.com>
2018-04-26 11:40:42 +0530

    Create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http=require('http');
 const path=require('path');
 const socketIO=require('socket.io');
 
+var {generateMessage}=require('./utils/message');
 const port=process.env.PORT || 3000;
 const rootURL=(port==3000) ?'http://localhost:3000/':'https://arcane-stream-39208.herokuapp.com/';
 const publicPath=path.join(__dirname,'../public');
@@ -11,24 +12,12 @@ var server=http.createServer(app);
 var io=socketIO(server);
 app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
-    socket.emit('newUser',{
-        from:'Admin',
-        text:'Welcome to the chat room',
-        createdAt: new Date().getTime()
-    });
-    socket.broadcast.emit('userJoined',{
-        from:'Admin',
-        text:'New user joined',
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newUser',generateMessage('Admin','Welcome to the chat room'));
+    socket.broadcast.emit('userJoined',generateMessage('Admin','New user joined'));
     socket.on('createMessage',(newMessage)=>{
         console.log('Created Message: ',newMessage);
         
-        io.emit('newMessage',{
-            from:newMessage.from,
-            text:newMessage.text,
-            createdAt:new Date().getTime()
-        });
+        io.emit('newMessage',generateMessage(newMessage.from,newMessage.text));
         // socket.broadcast.emit('newMessage',{
         //     from:newMessage.from,
         //     text:newMessage.text,
