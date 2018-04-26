bc73d77266edc1fd51126f9a6d1e46e91f98deec
Mukul Jaggi <mkujaggi@gmail.com>
2018-04-26 23:59:43 +0530

    Add mustachejs templates from message rendering

--- public/js/index.js
+++ public/js/index.js
@@ -8,19 +8,34 @@ socket.on('disconnect',function(){
 
 socket.on('newMessage',function(message){
     var formattedTime=moment(message.createdAt).format('h:mm a');
-    console.log('new Message.',message);
-    var li=$('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-    $('#messages').append(li);
+    var template=$('#message-template').html();
+    var html=Mustache.render(template,{
+        text:message.text,
+        from:message.from,
+        createdAt:formattedTime
+    });
+    $('#messages').append(html);
+    
+    // console.log('new Message.',message);
+    // var li=$('<li></li>');
+    // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+    // $('#messages').append(li);
 });
 socket.on('newLocationMessage',function(message){
-    var formattedTime=moment(message.createdAt).format('h:mm a'); 
-    var li=$('<li></li>');
-    var a=$('<a target="_blank">My current locatoin</a>')
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href',message.url)
-    li.append(a);
-    $('#messages').append(li);
+    var formattedTime=moment(message.createdAt).format('h:mm a');
+    var template=$('#locationMessage-template').html();
+    var html=Mustache.render(template,{
+        from:message.from,
+        createdAt:formattedTime,
+        url:message.url
+    });
+    $('#messages').append(html);
+    // var li=$('<li></li>');
+    // var a=$('<a target="_blank">My current locatoin</a>')
+    // li.text(`${message.from} ${formattedTime}: `);
+    // a.attr('href',message.url)
+    // li.append(a);
+    // $('#messages').append(li);
 });
 $(document).on('submit','#message-form',function(e){
     e.preventDefault();
